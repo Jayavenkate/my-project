b5c0bac2956add1c03d6159e92ab9aa5db48ed80
Jayavenkate <jayajaikithuja@gmail.com>
2023-08-25 00:18:48 +0300

    vercel

--- config/plugins.js
+++ config/plugins.js
@@ -3,4 +3,21 @@ module.exports = ({ env }) => ({
     enabled: true,
     // resolve: "./src/plugins/strapi-plugin-ckeditor",
   },
+  "vercel-deploy": {
+    enabled: true,
+  },
+  config: {
+    deployHook:
+      "https://api.vercel.com/v1/integrations/deploy/prj_sXlUOjgqNRDasOkqMAYxDEF8niUP/c6JjUpRHZu",
+    apiToken: "aru5BlkVKNSd3ZJsdXe1B4SH",
+    appFilter: "my-project",
+    teamFilter: "prj_sXlUOjgqNRDasOkqMAYxDEF8niUP",
+    roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+  },
 });
+
+// module.exports = ({ env }) => ({
+//   "vercel-deploy": {
+//     enabled: true,
+//   },
+// });
